787c6ce3bfd2bbf9fdefe01e0aacc9d019a6038c
Allen-Mikhailov <mikhailovallen@gmail.com>
2025-09-18 20:03:12 -0500

    Started work on shift register library

---
+++ ShiftRegisterTest/ShiftRegisterTest.ino
@@ -0,0 +1,73 @@
+const int SER = 13;
+const int OE = 25;
+const int RCLK = 14;
+
+const int SRCLK = 27;
+const int SRCLR = 26;
+
+const int STEP = 33;
+
+class ShiferRegister {
+  public:
+    int SER_PIN, OE_PIN, RCLK_PIN, SRCLK_PIN, SRCLR_PIN;
+    int output_count = 8;
+
+    void setup()
+    {
+      pinMode(SER_PIN, OUTPUT);
+      pinMode(OE_PIN, OUTPUT);
+      pinMode(RCLK_PIN, OUTPUT);
+      pinMode(SRCLK, OUTPUT);
+      pinMode(SRCLR, OUTPUT);
+    }
+
+    void setPins(int *pins)
+    {
+      digitalWrite(OE_PIN, LOW);
+      digitalWrite(SRCLR_PIN, HIGH);
+      for (int i = 0; i < output_count; i++)
+      {
+        if (pins[output_count-i-1] == 1)
+          digitalWrite(SER_PIN, HIGH);
+        else
+          digitalWrite(SER_PIN, LOW);
+
+        delay(10);
+        digitalWrite(SRCLK_PIN, HIGH);
+        delay(10);
+        digitalWrite(SRCLK_PIN, LOW);
+
+        delay(10);
+        digitalWrite(RCLK_PIN, HIGH);
+        delay(10);
+        digitalWrite(RCLK_PIN, LOW);
+
+        
+      }
+    }
+};
+
+ShiferRegister reg;
+
+void setup() {
+  // put your setup code here, to run once:
+  reg.SER_PIN = SER;
+  reg.OE_PIN = OE;
+  reg.SRCLK_PIN = SRCLK;
+  reg.RCLK_PIN = RCLK;
+  reg.SRCLR_PIN = SRCLR;
+
+  reg.setup();
+
+  int pins[] = {0, 0, 1, 1, 0, 0, 0, 0};
+  reg.setPins(pins);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(STEP, HIGH);
+  delay(10);
+  digitalWrite(STEP, LOW);
+  delay(10);
+}

1cc5e3c276ed41980632f17d354c2082627a283e
Hannah G <hg70@rice.edu>
2025-12-02 19:57:52 -0600

    Update Rover_DrivetrainHallEffectRotaryEncoderTest.ino
    Added RPM

--- Rover_DrivetrainHallEffectRotaryEncoderTest/Rover_DrivetrainHallEffectRotaryEncoderTest.ino
+++ Rover_DrivetrainHallEffectRotaryEncoderTest/Rover_DrivetrainHallEffectRotaryEncoderTest.ino
@@ -2,6 +2,11 @@
 #define DT 33
 #define CLK 32
 
+int clicks_per_rotation = 5000;
+
+float lastClick = 0;
+float currentClick = 0;
+float RPM = 0;
 int counter = 0;
 int currentStateCLK;
 int lastStateCLK;
@@ -35,16 +40,19 @@ void loop() {
     if (digitalRead(DT) != currentStateCLK) {
       counter --;
       currentDir ="CCW";
+      currentClick = millis();
     } else {
       // Encoder is rotating CW so increment
       counter ++;
       currentDir ="CW";
+      currentClick = millis();
     }
-
+    RPM = 60000/(clicks_per_rotation*(currentClick - lastClick)); //1 click/[change in time] ms * 1 rotation/[clicks per rotation] * 1000ms/1s * 60s/1min
     Serial.print("Direction: ");
     Serial.print(currentDir);
     Serial.print(" | Counter: ");
     Serial.println(counter);
+    lastClick = currentClick;
   }
 
   // Remember last CLK state

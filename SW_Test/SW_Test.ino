b2c581cb6cb280a07c3765e4efb0b96b24de572d
Hannah G <hg70@rice.edu>
2025-12-13 16:25:03 -0600

    Create SW_Test.ino

---
+++ SW_Test/SW_Test.ino
@@ -0,0 +1,60 @@
+#include <SerialWombat.h>
+
+SerialWombatChip sw;    //Declare a Serial Wombat chip
+
+//we're using a quadrature encoder with built-in pullup resistors
+SerialWombatQuadEnc qeWithPullUps(sw);
+
+//PWM at frequency of 31250 Hz.  Can change this probably if needed but we haven't figured out how yet
+SerialWombatPWM pwm(sw);
+
+
+
+int PWM = 1;
+int QE1 = 2;
+int QE2 = 3;
+int enableBoth = 4;
+int checkStall = 5;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+    Wire.begin(21, 22); // Initialize I2C with custom pin
+    Wire.setTimeOut(5000);
+	delay(500);
+	uint8_t i2cAddress = 0x60; //sw.find();
+    sw.begin(Wire,i2cAddress);  //Initialize the Serial Wombat library to use the primary I2C port
+	// sw.registerErrorHandler(SerialWombatSerialErrorHandlerBrief); //Register an error handler that will print communication errors to Serial
+
+   sw.queryVersion();
+  Serial.println();
+  Serial.print("Version "); Serial.println((char*)sw.fwVersion);
+  Serial.println("SW18AB Found.");
+
+  //begin(pin, starting duty cycle, inverted true/false)
+  pwm.begin(PWM, 0, false);
+  qeWithPullUps.begin(QE1, QE2);  // Initialize a QE on pins 2 and 3  Change these to different pins (say 18 and 19) on the SW18AB, since 3 is SDA
+
+
+  //digital enable pins
+  sw.pinMode(enableBoth, OUTPUT);
+  sw.pinMode(checkStall, INPUT);
+
+  //enable motor
+  sw.digitalWrite(enableBoth, LOW);
+  //spin motor
+  pwm.writeDutyCycle(3000);
+
+  analogWrite(33, 100);
+}
+
+void loop() {
+  //return;
+  Serial.println(qeWithPullUps.read());
+
+  if(sw.digitalRead(checkStall) == HIGH){
+    Serial.println("Stalling");
+  }
+  delay(50);
+}

f7e0ca0ccd552a9e899a4aa4c3749c90aaff8c0d
Hannah G <hg70@rice.edu>
2026-01-15 17:48:39 -0600

    misc

--- SW_Test/SW_Test.ino
+++ SW_Test/SW_Test.ino
@@ -1,6 +1,22 @@
 #include <SerialWombat.h>
 
+
+//for ESP32 RE reading:
+#define DT GPIO_NUM_33
+#define CLK GPIO_NUM_32
+float RPM;
+int currentStateCLK;
+int lastStateCLK;
+float counter;
+float lastCounter;
+float lastPrint;
+int clicks_per_rotation = 5264;
+//------------------------
+
 SerialWombatChip sw;    //Declare a Serial Wombat chip
+
+int newval;
+int oldval;
 
 //we're using a quadrature encoder with built-in pullup resistors
 SerialWombatQuadEnc qeWithPullUps(sw);
@@ -11,8 +27,8 @@ SerialWombatPWM pwm(sw);
 
 
 int PWM = 1;
-int QE1 = 2;
-int QE2 = 3;
+int QE1 = 7;
+int QE2 = 6;
 int enableBoth = 4;
 //int checkStall = 5;
 
@@ -39,7 +55,7 @@ void setup() {
 
   //begin(pin, starting duty cycle, inverted true/false)
   pwm.begin(PWM, 0, false);
-  qeWithPullUps.begin(QE1, QE2);  // Initialize a QE on pins 2 and 3
+  qeWithPullUps.begin(QE1, QE2, 0.01, true, QE_ONHIGH_INT);  // Initialize a QE on pins 2 and 3
 
 
   //digital enable pins
@@ -50,18 +66,79 @@ void setup() {
   sw.digitalWrite(enableBoth, HIGH);
   Serial.print("enabled motor");
   //spin motor
-  pwm.writeDutyCycle(3000);
+  pwm.writeDutyCycle(30000);
   Serial.print("Started motor");
 
+  //for using esp32:
+  pinMode(CLK, INPUT);
+  pinMode(DT, INPUT);
+
+  // Read the initial state of CLK
+  lastStateCLK = gpio_get_level(CLK);
+
+
   //analogWrite(33, 100);
+  oldval = 0;
 }
 
 void loop() {
+  
   //return;
-  //Serial.println(qeWithPullUps.read());
+
+  newval = qeWithPullUps.read();
+  if(newval!=oldval){
+    Serial.println(newval);
+    oldval = newval;
+  }
 
 //  if(sw.digitalRead(checkStall) == HIGH){
 //    Serial.println("Stalling");
  // }
   delay(50);
+
+
+
+
+
+  //---------------------------------------------------------- Reading REs with ESP32:
+  /*
+
+   // Read the current state of CLK
+  currentStateCLK = gpio_get_level(CLK);
+
+  // If last and current state of CLK are different, then pulse occurred
+  // React to only 1 state change to avoid double count
+  if (currentStateCLK != lastStateCLK) {
+
+
+    // If the DT state is different than the CLK state then
+    // the encoder is rotating CCW so decrement
+    if (gpio_get_level(DT) != currentStateCLK) {
+      counter--;
+    } else {
+      // Encoder is rotating CW so increment
+      counter++;
+    }
+    
+  }
+  
+  if ((millis() - lastPrint) > 1000) {
+        //RPM = [# of clicks]/[change in time, ms] * 1 rotation/[clicks per rotation] * 1000ms/1s * 60s/1min
+        //currently, this is actually rotation per second
+        RPM = 1000 * (counter - lastCounter) / (clicks_per_rotation * (millis() - lastPrint));
+        Serial.print(" | Counter: ");
+        Serial.print(counter);
+        Serial.print(" | RPS: ");
+        Serial.println(RPM);
+        lastPrint = millis();
+        lastCounter = counter;
+      }
+
+  // Remember last CLK state
+  lastStateCLK = currentStateCLK;
+
+  // Put in a slight delay to help debounce the reading
+
+  //------------------------------------------------------------------------------------------------------------------------------
+*/
 }

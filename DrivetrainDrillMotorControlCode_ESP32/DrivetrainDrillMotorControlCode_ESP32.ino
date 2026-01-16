b47770d24e7e84281e94d310e5389ba413421617
Hannah G <hg70@rice.edu>
2026-01-15 21:35:54 -0600

    movement library in progress

--- DrivetrainDrillMotorControlCode_ESP32/DrivetrainDrillMotorControlCode_ESP32.ino
+++ DrivetrainDrillMotorControlCode_ESP32/DrivetrainDrillMotorControlCode_ESP32.ino
@@ -1,29 +1,134 @@
-#import movementLibrary
+#include "movementLibrary.ino"
+
 
 //use this sketch to run main communication with arduino and call appropriate move functions
 
-/*move functions:
-drive forward: df()
-drive backward: db()
-stop: stop()
-turn right: tr()
-turn left: tl()
+  /*Instructions that can be sent by Jetson:
+    0 = stop rover
+    1 = rover go forward
+    2 = rover go backward
+    3 = rover turn right
+    4 = rover turn left
+    5 = drill forward
+    6 = drill backward
+    7 = drill stop
+    8 = m1 forward
+    9 = m1 backward
+    10 = m1 stop
+    11 = m2 forward
+    12 = m2 backward
+    13 = m2 stop
+    14 = m3 forward
+    15 = m3 backward
+    16 = m3 stop
+    17 = m4 forward
+    18 = m4 backward
+    19 = m4 stop
+  */
 
-drill forward: drillf()
-drill backward: drillb()
-
-can add more if desired
-*/
+bool newcommand = false;
+bool stalled = false;
+int currentcommand = 10;
+int message = 10;
 
 void setup() {
   // put your setup code here, to run once:
 
-  //call this at beginning to get sw chips set up from movement library
+  //initialize sw chips (from movement library)
   void initializeSW();
+
+  Serial.begin(115200);
+  
+  //NEED to set up serial communication between ESP32 and Jetson
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+
+  //NEED code to get instructions from Jetson
+  message = //new command from Jetson
+  if(message != currentcommand){
+    currentcommand = message;
+    newcommand = true;
+    stalled = false; 
+  }
+
+  //stalled cannot be set to true if newcommand is true
+  //if newcommand is true, stalled just updates RE status
+  stalled = checkStall(newcommand, currentcommand);
+
+  if(newcommand){
+    newcommand = false;
+    //do correct motor thing using command
+      if(currentcommand==0){
+        stop();
+      }
+      if(currentcommand==1){
+        df();
+      }
+      if(currentcommand==2){
+        db();
+      }
+      if(currentcommand==3){
+        tr();
+      }
+      if(currentcommand==4){
+        tl();
+      }
+      if(currentcommand==5){
+        r(0, drillSpeed);
+      }
+      if(currentcommand==6){
+        l(0, drillSpeed);
+      }
+      if(currentcommand==7){
+        s(0);
+      }
+      if(currentcommand==8){
+        r(1, motorSpeed);
+      }
+      if(currentcommand==9){
+        l(1, motorSpeed);
+      }
+      if(currentcommand==10){
+        s(1);
+      }
+      if(currentcommand==11){
+        r(2, motorSpeed);
+      }
+      if(currentcommand==12){
+        l(2, motorSpeed);
+      }
+      if(currentcommand==13){
+        s(2);
+      }
+            if(currentcommand==14){
+        r(3, motorSpeed);
+      }
+      if(currentcommand==15){
+        l(3, motorSpeed);
+      }
+      if(currentcommand==16){
+        s(3);
+      }
+            if(currentcommand==17){
+        r(4, motorSpeed);
+      }
+      if(currentcommand==18){
+        l(4, motorSpeed);
+      }
+      if(currentcommand==19){
+        s(4);
+      }
+
+    
+    }
+    
+  if(stalled){
+    //send to the Jetson that it's stalled
+
+  }
+    //
+  }
 }
--- DrivetrainDrillMotorControlCode_ESP32/movementLibrary.ino
+++ DrivetrainDrillMotorControlCode_ESP32/movementLibrary.ino
@@ -1,117 +1,283 @@
 //make all of the move functions here to be called by main program
-//UNFINISHED
-//need to do sw syntax in initializeSW(), r(), l(), and s().
-//need to add ability to control motor speed via r(), l(), and s() if desired.
-//need to add rotary encoders/speed monitoring and control in drillf(), drillb(), df(), db(), tr(), and tl() if desired.
-//need to add motor stall prevention using current alarms if desired.
+#include <SerialWombat.h>
+
+//speed of all driving motors:
+int driveSpeed = 30000;
+//speed of drill:
+int drillSpeed = 30000;
+
+//-------------starting definitions:
+  //for rotary encoder stall tracking:
+  int CLK0 = 0;
+  int CLK1 = 0;
+  int CLK2 = 0;
+  int CLK3 = 0;
+  int CLK4 = 0;
+
+  //sw pins setup:
+  int RIS = 1;
+  int REN = 2;
+  int RPWM = 3;
+  int LPWM = 4;
+  int LEN = 5;
+  int LIS = 6;
+  int CHA = 7;
+  int CHB = 8;
+
+  //initialize sw chips
+  SerialWombatChip motor0;
+  SerialWombatChip motor1;
+  SerialWombatChip motor2;
+  SerialWombatChip motor3;
+  SerialWombatChip motor4;
+
+  //initialize REs
+  SerialWombatQuadEnc RE0(motor0);
+  SerialWombatQuadEnc RE1(motor1);
+  SerialWombatQuadEnc RE2(motor2);
+  SerialWombatQuadEnc RE3(motor3);
+  SerialWombatQuadEnc RE4(motor4);
+
+  //initialize PWMs
+  SerialWombatPWM RPWM0(motor0);
+  SerialWombatPWM LPWM0(motor0);
+  SerialWombatPWM RPWM1(motor1);
+  SerialWombatPWM LPWM1(motor1);
+  SerialWombatPWM RPWM2(motor2);
+  SerialWombatPWM LPWM2(motor2);
+  SerialWombatPWM RPWM3(motor3);
+  SerialWombatPWM LPWM3(motor3);
+  SerialWombatPWM RPWM4(motor4);
+  SerialWombatPWM LPWM4(motor4);
 
 
-//sw pins setup:
-int RIS = 1;
-int REN = 2;
-int RPWM = 3;
-int LPWM = 4;
-int LEN = 5;
-int LIS = 6;
 
-/*//uncomment if using REs through sw chip:
-int CLK = 7;
-int DT = 8;
+//-------------setup: must be called at the beginning of main program in order for the rest of the commands to work 
+  void initializeSW(){
+    //initialize sw chip and pins for each motor (motor numbers printed on PCB)
 
-//if using ESP-32 to manage REs, need to copy pin connection names for REs 0--4
-*/
+    //start i2C
+    Wire.begin(21, 22); // Initialize I2C with custom pin
+    Wire.setTimeOut(5000);
+    delay(500);
 
+    //set sw addresses
+    uint8_t i2cAddress0 = 0x60;
+    uint8_t i2cAddress1 = 0x61;
+    uint8_t i2cAddress2 = 0x62;
+    uint8_t i2cAddress3 = 0x63;
+    uint8_t i2cAddress4 = 0x64;
 
+    //Initialize sws on I2C
+    motor0.begin(Wire,i2cAddress0);
+    motor1.begin(Wire,i2cAddress1);
+    motor2.begin(Wire,i2cAddress2);
+    motor3.begin(Wire,i2cAddress3);
+    motor4.begin(Wire,i2cAddress4);
 
-//-------------setup: must be called at the beginning of main program in order for the rest of the commands to work
-void initializeSW(){
-  //create sw chip for each motor, named motor0, motor1, etc.
-  //numbers corresponding to each motor are printed on PCB!!
-  for(int i=0; i<5; i++){
-    str name = "motor" + str(i);
-    //create new sw chip named "name"
-    //set i2c address of chip equal to 60 + i
-    //set pinmodes for each pin (RIS & LIS: input, REN & LEN: output, RPWM & LPWM: PWM, DT & CLK: encoders)
-    //start i2c communication with sw chip
+    //begin PWM pins
+    RPWM0.begin(RPWM, 0, false);
+    LPWM0.begin(LPWM, 0, false);
+    RPWM1.begin(RPWM, 0, false);
+    LPWM1.begin(LPWM, 0, false);
+    RPWM2.begin(RPWM, 0, false);
+    LPWM2.begin(LPWM, 0, false);
+    RPWM3.begin(RPWM, 0, false);
+    LPWM3.begin(LPWM, 0, false);
+    RPWM4.begin(RPWM, 0, false);
+    LPWM4.begin(LPWM, 0, false);
+    
+    //set up enable pins
+    motor0.pinMode(LEN, OUTPUT);
+    motor0.pinMode(REN, OUTPUT);
+    motor1.pinMode(LEN, OUTPUT);
+    motor1.pinMode(REN, OUTPUT);
+    motor2.pinMode(LEN, OUTPUT);
+    motor2.pinMode(REN, OUTPUT);
+    motor3.pinMode(LEN, OUTPUT);
+    motor3.pinMode(REN, OUTPUT);
+    motor4.pinMode(LEN, OUTPUT);
+    motor4.pinMode(REN, OUTPUT);
+
+    //set up current alarms
+    motor0.pinMode(LIS, INPUT);
+    motor0.pinMode(RIS, INPUT);
+    motor1.pinMode(LIS, INPUT);
+    motor1.pinMode(RIS, INPUT);
+    motor2.pinMode(LIS, INPUT);
+    motor2.pinMode(RIS, INPUT);
+    motor3.pinMode(LIS, INPUT);
+    motor3.pinMode(RIS, INPUT);
+    motor4.pinMode(LIS, INPUT);
+    motor4.pinMode(RIS, INPUT);
+
+    //initialize rotary encoders
+    RE0.begin(CHA, CHB, 0.01, true, QE_ONHIGH_INT);
+    RE1.begin(CHA, CHB, 0.01, true, QE_ONHIGH_INT);
+    RE2.begin(CHA, CHB, 0.01, true, QE_ONHIGH_INT);
+    RE3.begin(CHA, CHB, 0.01, true, QE_ONHIGH_INT);
+    RE4.begin(CHA, CHB, 0.01, true, QE_ONHIGH_INT);
+
+    //enable all motors
+    motor0.digitalWrite(LEN, HIGH);
+    motor0.digitalWrite(REN, HIGH);
+    motor1.digitalWrite(LEN, HIGH);
+    motor1.digitalWrite(REN, HIGH);
+    motor2.digitalWrite(LEN, HIGH);
+    motor2.digitalWrite(REN, HIGH);
+    motor3.digitalWrite(LEN, HIGH);
+    motor3.digitalWrite(REN, HIGH);
+    motor4.digitalWrite(LEN, HIGH);
+    motor4.digitalWrite(REN, HIGH);
   }
-}
 
 //------------subcommands: will not be called in main program, but used to build commands
 
-//turns any motor right
-void r(str motorname){
-  s(motorname);
-  //set pins on sw chip to turn motor R
-}
+  //turns any motor right
+  void r(str motornum, dutyCycle){
+    s(motorname);
+    str RightPWM = "RPWM" + motornum;
+    RightPWM.writeDutyCycle(dutyCycle);
+  }
 
-//turns any motor left
-void l(str motorname){
-  s(motorname);
-  //set pins on sw chip to turn motor  L
-}
+  //turns any motor left
+  void l(str motornum, dutyCycle){
+    s(motornum);
+    str LeftPWM = "LPWM" + motornum;
+    LeftPWM.writeDutyCycle(dutyCycle);
+  }
 
-//stops motor
-void s(str motorname){
-  //set pins on sw chip to stop motor
-}
+  //stops motor
+  void s(str motornum){
+    str RightPWM = "RPWM" + motornum;
+    str LeftPWM = "LPWM" + motornum;
+    RightPWM.writeDutyCycle(0);
+    RightPWM.writeDutyCycle(0);
+  }
 
 
-//------------commands: will be called in main program
+//------------commands: will be called in main program 
 
-//stops all drivetrain motors
-void stop(){
-  s(motor1);
-  s(motor2);
-  s(motor3);
-  s(motor4);
-}
+  //stops all drivetrain motors
+  void stop(){
+    s(1, driveSpeed);
+    s(2, driveSpeed);
+    s(3, driveSpeed);
+    s(4, driveSpeed);
+  }
 
-//turns drill forward. Can swap "r" with "l" if drill turns wrong direction
-void drillf(){
-  r(motor0);
-}
+  //makes rover drive forward. Can swap "r" with "l" if rover goes wrong direction
+  void df(){
+    stop()
+    r(1, driveSpeed);
+    r(2, driveSpeed);
+    l(3, driveSpeed);
+    l(4, driveSpeed);
+  }
 
-//turns drill backward. Can swap "r" with "l" if drill turns wrong direction
-void drillb(){
-  l(motor0);
-}
+  //makes rover drive backward. Can swap "r" with "l" if rover goes wrong direction
+  void db(){
+    stop()
+    l(1, driveSpeed);
+    l(2, driveSpeed);
+    r(3, driveSpeed);
+    r(4, driveSpeed);
+  }
 
-//makes rover drive forward. Can swap "r" with "l" if rover goes wrong direction
-df(){
-  stop()
-  r(motor1);
-  r(motor2);
-  l(motor3);
-  l(motor4);
-}
+  //makes rover turn right. Can swap "r" with "l" if rover goes wrong direction
+  void tl(){
+    stop()
+    l(1, driveSpeed);
+    l(2, driveSpeed);
+    l(3, driveSpeed);
+    l(4, driveSpeed);
+  }
 
-//makes rover drive backward. Can swap "r" with "l" if rover goes wrong direction
-db(){
-  stop()
-  l(motor1);
-  l(motor2);
-  r(motor3);
-  r(motor4);
-}
+  //makes rover turn left. Can swap "r" with "l" if rover goes wrong direction
+  void tr(){
+    stop()
+    r(1, driveSpeed);
+    r(2, driveSpeed);
+    r(3, driveSpeed);
+    r(4, driveSpeed);
+  }
 
-//makes rover turn right. Can swap "r" with "l" if rover goes wrong direction
-tl(){
-  stop()
-  l(motor1);
-  l(motor2);
-  l(motor3);
-  l(motor4);
-}
 
-//makes rover turn left. Can swap "r" with "l" if rover goes wrong direction
-tl{
-  stop()
-  r(motor1);
-  r(motor2);
-  r(motor3);
-  r(motor4);
-}
 
+//------------checkStall and subcommands: a class all of its own 
+  bool checkStall(bool newcommand, int currentcommand){
+    bool stall;
+    if(newcommand){
+      //NEED TO UPDATE ALL GLOBAL ENCODER VALUES
+      return false;
+    }
+    else if(currentcommand == 0){
+        return false;
+      }
+    }
+    else if((currentcommand >0)&&(currentcommand<4)){
+      stall1 = getStall(1);
+      stall2 = getStall(2);
+      stall3 = getStall(3);
+      stall4 = getStall(4);
+      if(((stall1 == true)||(stall2==true))||((stall3==true)||(stall4==true))){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    else if((currentcommand >3)&&(currentcommand<8)){
+      stall = getStall(0);
+      return stall;
+    }
+    else if((currentcommand >7)&&(currentcommand<11)){
+      stall = getStall(1);
+      return stall;
+    }
+    else if((currentcommand >10)&&(currentcommand<14)){
+      stall = getStall(2);
+      return stall;
+    }
+    else if((currentcommand >13)&&(currentcommand<17)){
+      stall = getStall(3);
+      return stall;
+    }
+    else if((currentcommand >16)&&(currentcommand<20)){
+      stall = getStall(4);
+      return stall;
+    }
+  }
+
+  bool getStall(string motornum){
+    REname = "RE" + motornum;
+    int newCLK = REname.read();
+    int oldCLK = CLK(motornum);
+    if((oldCLK-newCLK != 0)){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+
+  int CLK(string motornum){
+    if(motornum==0){
+      return CLK0;
+    }
+    if(motornum==1){
+      return CLK1;
+    }
+    if(motornum==2){
+      return CLK2;
+    }
+    if(motornum==3){
+      return CLK3;
+    }
+    if(motornum==4){
+      return CLK4;
+    }
+  }
 
 
 
